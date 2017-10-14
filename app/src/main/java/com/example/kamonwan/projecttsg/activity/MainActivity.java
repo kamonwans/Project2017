9112309ec77f1733f6c2361ecd53b1bee0891c44
kamonwans <kamonwan294@gmail.com>
2017-10-14 13:44:33 +0700

    Change menu hamberger

--- app/src/main/java/com/example/kamonwan/projecttsg/activity/MainActivity.java
+++ app/src/main/java/com/example/kamonwan/projecttsg/activity/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.kamonwan.projecttsg.activity;
 
 import android.content.res.Configuration;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 import com.example.kamonwan.projecttsg.R;
-import com.example.kamonwan.projecttsg.adapter.MainTabBarAdapter;
 import com.example.kamonwan.projecttsg.fragment.MainMenuFragment;
-import com.example.kamonwan.projecttsg.fragment.TabBarFragment;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +28,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initInstance() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         FragmentMainMenu();
-        FragmentTabBar();
         actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,
                 R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("ThaiSpellingGame");
+
+
     }
 
     public void FragmentMainMenu() {
@@ -46,13 +50,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
-    public void FragmentTabBar() {
-        TabBarFragment tabBarFragment = new TabBarFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.contentMain, tabBarFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
